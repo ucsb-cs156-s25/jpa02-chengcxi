1e0444e0c368c11dfd3ea409e4498cd6b01cb7c4
chengcxi <cxi@ucsb.edu>
2025-04-06 16:36:41 -0700

    filled all tests

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -23,4 +23,40 @@ public class TeamTest {
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void equals_returns_true_for_same_object() {
+        assertEquals(true, team.equals(team));
+    }
+
+    @Test
+    public void equals_returns_false_for_different_object() {
+        Object other = "Invalid object";
+        assertEquals(false, team.equals(other));
+    }
+
+    @Test
+    public void equals_returns_false_for_different_name() {
+        Team other = new Team("other-team");
+        assertEquals(false, team.equals(other));
+    }
+
+    @Test
+    public void equals_returns_false_for_different_members() {
+        Team other = new Team("test-team");
+        other.addMember("other-member");
+        assertEquals(false, team.equals(other));
+    }
+
+    @Test
+    public void hashCode_returns_same_value_for_same_object() {
+        int result = team.hashCode();
+        int expected = -1226298695;
+        assertEquals(expected, result);
+    }
+
 }
